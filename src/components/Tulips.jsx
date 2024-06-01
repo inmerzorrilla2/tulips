87aa993e79a58e644e832c49b5780a5fb68c89f5
Inmer Zorrilla <elchamoinmer@gmail.com>
2024-05-31 23:07:35 -0600

    Cuarto Commit

--- src/components/Tulips.jsx
+++ src/components/Tulips.jsx
@@ -6,7 +6,7 @@ const Tulips = ({name, color, image}) => {
         <h2>{name}</h2>
         <p>{color}</p>
         <img src={image} alt={name} />
-        <p>****31 de mayo del 2024...</p>
+        {/* <p>****31 de mayo del 2024...</p> */}
     </section>
   )
 }
